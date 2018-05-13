8858ca98773149918e3110b31756fe088c2ab696
Anastasiya <shabanova27_00@mail.ru>
2018-05-13 18:36:17 +0300

    Регистрация

--- pages/login/login.js
+++ pages/login/login.js
@@ -1,17 +1,28 @@
  window.addEventListener("load", () => {
-     document.querySelector('.registration').onclick=function()
-    {
-    var username=document.querySelector('.login').value;
-    var res;               
-    apiRequest('/register', { method: 'POST', body: `username=${username}` })
-        .then(text => {
-            res = JSON.parse(text);               
-            if(res.status==='ok')
-            {         
-                setUserId({ username:res.user.username, token:res.user.id });                          
-                window.location = "/ready/ready.html";                  
-            }
-        });
-    }
- })
+     var old = getUser();
+     if(old){
+         apiRequest(`/whoami?user_id=${old.token}`)
+            .then(text => {
+                 res = JSON.parse(text);               
+                    if(res.status==='ok')
+                    {                                                        
+                     window.location = "/ready/ready.html";                  
+                    }
+            });    
+     }
+        document.querySelector('.registration').onclick=function()
+        {   
+            var username=document.querySelector('.login').value; 
+            var res;               
+            apiRequest('/register', { method: 'POST', body: `username=${username}` })
+                .then(text => {
+                 res = JSON.parse(text);               
+                    if(res.status==='ok')
+                    {         
+                        setUserId({ username:res.user.username, token:res.user.id });                          
+                     window.location = "/ready/ready.html";                  
+                    }
+            });
+        }
+     })
  
