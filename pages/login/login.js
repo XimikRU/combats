c5d0aff29e4003ecde812ad1b5ea817f01ed4386
XimikRU <ermachkov-ilya1999@mail.ru>
2018-05-15 20:00:12 +0300

    login page remade, minor fixes

--- pages/login/login.js
+++ pages/login/login.js
@@ -1,28 +1,30 @@
  window.addEventListener("load", () => {
      var old = getUser();
-     if(old){
+     if (old) {
          apiRequest(`/whoami?user_id=${old.token}`)
-            .then(text => {
-                 res = JSON.parse(text);               
-                    if(res.status==='ok')
-                    {                                                        
-                     window.location = "/ready/";                  
-                    }
-            });    
+             .then(text => {
+                 res = JSON.parse(text);
+                 if (res.status === 'ok') {
+                     window.location = "/ready/";
+                 }
+             });
      }
-        document.querySelector('.registration').onclick=function()
-        {   
-            var username=document.querySelector('.login').value; 
-            var res;               
-            apiRequest('/register', { method: 'POST', body: `username=${username}` })
-                .then(text => {
-                 res = JSON.parse(text);               
-                    if(res.status==='ok')
-                    {         
-                        setUserId({ username:res.user.username, token:res.user.id });                          
-                     window.location = "/ready/ready.html";                  
-                    }
-            });
-        }
-     })
- 
+
+     document.querySelector('.btn-login').onclick = function () {
+         var username = document.querySelector('.inp-username').value;
+         apiRequest('/register', {
+                 method: 'POST',
+                 body: `username=${username}`
+             })
+             .then(text => {
+                 var res = JSON.parse(text);
+                 if (res.status === 'ok') {
+                     setUserId({
+                         username: res.user.username,
+                         token: res.user.id
+                     });
+                     window.location = "/ready/";
+                 }
+             });
+     }
+ })
